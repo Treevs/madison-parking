a1326549be3f6813a55b0fbc1c4aaf1112b72ff7
Trevor Wilson <wilsontp14@uww.edu>
2019-07-11 19:09:40 -0500

    Deprecate app.js for now

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 4000
-const proxy = require('express-http-proxy')
+// const express = require('express')
+// const app = express()
+// const port = 4000
+// const proxy = require('express-http-proxy')
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/cityparking', proxy('https://www.cityofmadison.com/parking-utility/data/ramp-availability.json'))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+/*
+    DEPRECATED, BUT WILL BE USEFUL LATER
+*/
